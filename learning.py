3cf25914a815696bafa1277328ccc2ea4034ac4c
JasonRafeMiller <jmill02@shepherd.edu>
2023-04-29 10:18:45 -0400

    Notebook plus learning module.

---
+++ learning.py
@@ -0,0 +1,478 @@
+import numpy as np
+import pandas as pd
+import scipy.stats as ss
+import pickle
+from matplotlib import pyplot as plt 
+import sklearn   
+from tensorflow import keras
+from keras import models
+from sklearn.mixture import GaussianMixture
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import f1_score
+from sklearn.metrics import precision_recall_curve
+from sklearn.metrics import roc_curve
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import auc
+from sklearn.metrics import matthews_corrcoef
+from sklearn.utils import class_weight
+from sklearn.metrics import confusion_matrix
+from tensorflow import keras
+from keras.models import Sequential
+from keras.layers import TimeDistributed
+from keras.layers import Flatten
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.losses import BinaryCrossentropy
+from keras.callbacks import EarlyStopping
+
+class DataLoader():
+    
+    def __init__(self,K,alphabet):
+        self.cache=dict() 
+        self.vals = {'A':0, 'C':1, 'G':2, 'T':3}
+        self.gene2rci = dict()
+        self.K = K
+        self.alphabet = alphabet
+        
+    def load_gene_rci_values(self,filepath,exclusions):
+        '''
+        Load all the genes from the given RCI csv file.
+        The given file usually contains train or test, not both.
+        Load average RCI for each across cell lines.
+        Define average as log of mean of antilogs: log2(mean(2^RCI)).
+        Return dict with keys=gene:str and values=RCI:float.
+        '''
+        self.gene2rci = {}
+        overall_sum = 0
+        with open (filepath,'r') as handle:
+            for row in handle:
+                line = row.strip()
+                fields = line.split(',')
+                gene_id = fields.pop(0)
+                cell_line_index = 0
+                rci_values = []
+                log_mean=0.0
+                for rci_str in fields:
+                    if cell_line_index not in exclusions:
+                        if rci_str != "nan":
+                            rci_val = float(rci_str)
+                            rci_values.append(rci_val)
+                    cell_line_index += 1
+                if len(rci_values)>0:
+                    values = np.array(rci_values)
+                    antilogs = [2**x for x in values]
+                    big_mean = np.mean(antilogs)
+                    # Avoid division by zero
+                    if np.absolute(big_mean)<0.000001:
+                        log_mean = -1000000 # neg infinity
+                    else:
+                        log_mean = np.log2(big_mean) 
+                    self.gene2rci[gene_id] = log_mean
+        print('Number of RCI values loaded',len(self.gene2rci.keys()))
+        return self.gene2rci
+
+    def _seq_to_kmer_values(self,rna,K):
+        vec=[] # seq converted to list of K-mers 
+        N_indicator = 0 # indicator value
+        length = len(rna)
+        for i in range(length-K+1):
+            kmer = rna[i:i+K]
+            if 'N' in kmer:
+                value = N_indicator
+            elif kmer in self.cache.keys():
+                value = self.cache[kmer]
+            else:
+                value = 0
+                for j in range(K):
+                    value *= 4   
+                    nextnuc = kmer[j] 
+                    nucval = self.vals[nextnuc]
+                    value += nucval
+                value += 1   # NNN => 0, AAA => 1
+                self.cache[kmer] = value
+            vec.append(value)
+        return vec
+
+    def load_sequence(self,filepath):
+        '''
+        Load all the sequences from the given file. 
+        Load our version of GenCode -- csv, easier to parse than fasta.
+        Each line has IDs plus sequence.
+        The IDs in the file do not include dot-version numbers.
+        The file may already be filtered e.g. canonical transcripts.
+        '''
+        allids=[]
+        allseq=[]
+        #NREPEAT = str('N'*MAXLEN)   # not used for MLP
+        with open (filepath,'r') as handle:
+            header = None
+            for row in handle:
+                if header is None:
+                    header = row
+                else:
+                    line    = row.strip()
+                    fields  = line.split(',')
+                    tran_id = fields[0]  # without version number
+                    gene_id = fields[1]  # without version number
+                    seq_len = int(fields[3])
+                    seq_txt = fields[4]
+                    # Keep only transcripts having numeric RCI given the cell lines in use.
+                    # We have validated this by spot checking.
+                    # TO DO: validate this programmatically.
+                    if gene_id in self.gene2rci.keys():
+                        # no MAXLEN for MLP
+                        #if seq_len<=MAXLEN:
+                        #    seq_txt = seq_txt + NREPEAT
+                        #seq_txt = seq_txt[:MAXLEN]
+                        allids.append( (gene_id,tran_id) )
+                        hot_vec = self._seq_to_kmer_values(seq_txt,self.K)
+                        allseq.append(hot_vec)
+        self.cache=dict() # save RAM
+        return allids,allseq
+
+    def load_spectra(self,filepath):
+        '''
+        Load all (variable-length) sequences as lists of kmers.
+        Then convert each sequence to (fixed-length) kmer histograms.
+        '''
+        allids,allseq = self.load_sequence(filepath)
+        allspectra = []
+        for seq in allseq:
+            spectrum = np.zeros(self.alphabet)
+            for kmer in seq:
+                spectrum[kmer] += 1
+            spectrum /= len(seq)
+            allspectra.append(spectrum)
+        return allids,allspectra        
+
+class ModelBuilder():
+    def __init__(self,alphabet):
+        self.alphabet = alphabet
+        
+    def build_model(self):
+        mlp    = Sequential()
+        layer1 = Dense(128, activation='sigmoid',dtype='float32',input_dim=self.alphabet)
+        layer2 = Dense(64, activation='sigmoid')
+        drop2  = Dropout(.2)
+        layer3 = Dense(64, activation='sigmoid')
+        drop3  = Dropout(.2)
+        output = Dense(1, activation='sigmoid')
+        mlp.add(layer1)
+        mlp.add(layer2)
+        mlp.add(drop2)
+        mlp.add(layer3)
+        mlp.add(drop3)
+        mlp.add(output)
+        bc=BinaryCrossentropy(from_logits=False)
+        mlp.compile(loss=bc, optimizer="Adam", metrics=["accuracy"])
+        return mlp
+
+class CrossValidator():
+
+    def __init__(self,epochs,rci_threshold=0,score_threshold=0.5):
+        self.epochs = epochs
+        self.discriminator = rci_threshold
+        self.score_threshold = score_threshold
+        self.mechanism = 'ZERO'
+        self.flip = False
+        self.reset_statistics()
+        
+    def set_model_builder(self,builder):
+        self.model_builder = builder
+        
+    def reset_statistics(self):
+        self.cv_accuracy=[]
+        self.cv_precision=[]
+        self.cv_recall=[]
+        self.cv_f1=[]
+        self.cv_auprc=[]
+        self.cv_auroc=[]
+        self.cv_mcc=[]
+        
+    def _get_X_y(self, all_ids, all_seqs, rci_map): 
+        # Prepare X and y for training or testing.
+        subsetX=[]
+        subsetY=[]
+        for t in range(len(all_ids)):
+            gene_id,tran_id = all_ids[t]
+            oneX            = all_seqs[t]
+            oneY            = rci_map[gene_id]
+            subsetX.append(oneX)
+            subsetY.append(oneY)
+        subsetX = np.array(subsetX)
+        subsetY = np.array(subsetY).reshape((-1,1))
+        return subsetX,subsetY
+    
+    def set_threshold_mechanism(self, mechanism):
+        if mechanism not in ['RCI_GMM','THE_MEAN','ZERO']:
+            raise Exception('Unrecognized mechansm:',mechanism)
+        self.mechanism = mechanism
+    
+    def _apply_threshold(self, array_of_rci):
+        # Takes list of float, returns list of labels [0,1].
+        if self.mechanism == 'RCI_GMM':
+            labels = self.discriminator.predict(array_of_rci)
+            if self.flip:
+                IS_CYTO = lambda label: 1 if label==0 else 0
+                labels = np.array(list(map(IS_CYTO, labels)))
+        else:  # 'THE_MEAN' or 'ZERO'
+            rci_threshold = self.discriminator
+            IS_CYTO = lambda rci: 1 if rci>rci_threshold else 0
+            labels = np.array(list(map(IS_CYTO, array_of_rci)))
+        return labels
+    
+    def _prepare_threshold(self, rci_values, create=True):
+        if self.mechanism == 'RCI_GMM':
+            if create:  # during training, create a new GMM
+                gmm = GaussianMixture(n_components=2, verbose=0, 
+                  covariance_type='spherical', n_init=100) # random_state=42) 
+                gmm.fit(rci_values)
+            else:   # during testing, use existing GMM
+                gmm=self.discriminator
+            self.flip = False
+            # The GMM labels are arbitrary.
+            if gmm.means_[0][0] > gmm.means_[1][0]:
+                self.flip = True
+            self.discriminator = gmm   # redundant but consistent
+        elif self.mechanism == 'THE_MEAN':
+            self.discriminator = np.mean(rci_values)
+        elif self.mechanism == 'ZERO':
+            self.discriminator = RCI_THRESHOLD_VALUE
+        else: # not expected
+            self.discriminator = 0
+    
+    def _explain_threshold(self):
+        if self.mechanism == 'RCI_GMM':
+            gmm=self.discriminator
+            print('Discriminator is GMM')
+            print('Means',[gmm.means_[0][0],gmm.means_[1][0]])
+            print('Variances',gmm.covariances_)
+            print('Priors',gmm.weights_)
+            test_rcis=[-5,-4,-3.5,-3,-2.5,-2,-1.5,-1,-0.5,0,0.5,1,1.5,2,3]
+            print(test_rcis)
+            print(self._apply_threshold(np.array(test_rcis).reshape((-1,1))))
+        else:
+            print('Discriminator',self.mechanism,self.discriminator)
+    
+    def _show_sizes(self,label,values):
+        a = np.count_nonzero(values==1)
+        b = np.count_nonzero(values==0)
+        print('%s 1:0 %d:%d %5.2f%%'%(label,a,b,100*a/(a+b)))
+        
+    def save_model(self,filename1,filename2):
+        if self.model is not None:
+            filepath = MODEL_DIR + filename1
+            self.model.save(filepath)
+            print('Saved model 1 to',filepath)
+        if self.discriminator is not None:
+            filepath = MODEL_DIR + filename2
+            pickle.dump(self.discriminator, open(filepath, 'wb'))
+            print('Saved model 2 to',filepath)
+   
+    def load_model(self,filename1,filename2):
+        filepath = MODEL_DIR + filename1
+        self.model = keras.models.load_model(filepath)
+        print('Loaded model 1 from',filepath)
+        filepath = MODEL_DIR + filename2
+        self.discriminator = pickle.load(open(filepath, 'rb'))        
+        print('Loaded model 2 from',filepath)
+        
+    def train_new_model(self,train_ids,train_seq,train_rci,
+            valid_ids=None,valid_seq=None,valid_rci=None):
+        X_train,y_rci = self._get_X_y(train_ids,train_seq,train_rci) 
+        self._prepare_threshold(y_rci,True)  
+        self._explain_threshold()
+        y_train = self._apply_threshold(y_rci)
+        self._show_sizes('Train',y_train)
+        cw = class_weight.compute_class_weight('balanced', classes=[0,1], y=y_train)
+        cwd = {0: cw[0], 1: cw[1]}
+        print('Computed class weights:',cwd)
+        if valid_ids is not None:
+            X_valid,y_rci = self._get_X_y(valid_ids,valid_seq,valid_rci) 
+            y_valid = self._apply_threshold(y_rci)
+            self._show_sizes('Valid',y_valid)
+        y_rci = None
+
+        self.model=self.model_builder.build_model()
+        print("FIT")
+        callback = EarlyStopping(monitor='val_accuracy',
+          patience=100, start_from_epoch=100, restore_best_weights=True)
+        if valid_ids is None:
+            history=self.model.fit(X_train, y_train, # batch_size=10, default=32 works nicely
+            epochs=self.epochs, verbose=0, class_weight=cwd, callbacks=[callback])
+            # no validation 
+        else:
+            history=self.model.fit(X_train, y_train, # batch_size=10, default=32 works nicely
+            epochs=self.epochs, verbose=0,  # verbose=1 for ascii art, verbose=0 for none 
+            validation_data=(X_valid,y_valid), class_weight=cwd, callbacks=[callback])
+
+        pd.DataFrame(history.history).plot(figsize=(8,5))
+        plt.grid(True)
+        plt.gca().set_ylim(0,1)
+        plt.show()
+        
+    def test_without_training(self,test_ids,test_seq,test_rci):
+        # For final test, do no train.
+        # Assume set_sequences() set the test set.
+        X_test,y_rci = self._get_X_y(test_ids,test_seq,test_rci) 
+        y_test = self._apply_threshold(y_rci)
+        y_rci = None
+        
+        print("PREDICT")
+        yhat_pred=self.model.predict(X_test, verbose=0)     
+        yhat_classes=np.where(yhat_pred > self.score_threshold, 1, 0)
+
+        self._show_sizes('Test',y_test)
+        self._show_sizes('Predict',yhat_classes)
+        print('Test sizes',X_test.shape,y_test.shape)
+        print('Distrib of scores:',np.mean(yhat_pred),'mean',np.std(yhat_pred),'std')
+        print('Range of scores:',np.min(yhat_pred),'to',np.max(yhat_pred))
+        print('Score threshold',self.score_threshold)
+        cm1 = confusion_matrix(y_test,yhat_classes)
+        print('Confusion matrix\n',cm1)
+        cm2 = confusion_matrix(y_test,yhat_classes,normalize='all')
+        print('Normalized matrix\n',cm2)
+
+        accuracy = accuracy_score(y_test, yhat_classes)*100.
+        precision = precision_score(y_test, yhat_classes)*100.
+        recall = recall_score(y_test, yhat_classes)*100.
+        f1 = f1_score(y_test, yhat_classes)*100.
+        prc_Y, prc_X, prc_bins = precision_recall_curve(y_test, yhat_pred)
+        auprc = auc(prc_X,prc_Y)*100.
+        auroc = roc_auc_score(y_test, yhat_pred)*100.
+        mcc = matthews_corrcoef(y_test, yhat_classes)
+
+        correct_pos = 0
+        correct_neg = 0
+        wrong_pos = 0
+        wrong_neg = 0
+        for i in range(len(y_test)):
+            if yhat_pred[i]>=0.65:
+                if y_test[i]==1:
+                    correct_pos += 1
+                else:
+                    wrong_pos += 1
+            elif yhat_pred[i]<=0.35:
+                if y_test[i]==0:
+                    correct_neg += 1
+                else:
+                    wrong_neg += 1
+        print('Extreme scores correct, pos:neg',correct_pos,correct_neg)  
+        print('Extreme scores incorrect pos:neg',wrong_pos,wrong_neg)  
+
+        self._pretty_pictures(y_test,yhat_pred,prc_X,prc_Y)
+        print('Accuracy: %.2f%% Precision: %.2f%% Recall: %.2f%%' % (accuracy,precision,recall)) 
+        print('F1: %.2f%% MCC: %.4f' % (f1,mcc)) 
+        print('AUPRC: %.2f%% AUROC: %.2f%%' % (auprc,auroc)) 
+
+        self.cv_accuracy.append(accuracy)
+        self.cv_precision.append(precision)
+        self.cv_recall.append(recall)
+        self.cv_f1.append(f1)
+        self.cv_mcc.append(mcc)
+        self.cv_auprc.append(auprc)
+        self.cv_auroc.append(auroc)
+
+    def _pretty_pictures(self,y_valid,yhat_pred,prc_X,prc_Y):
+        count_ones= len(y_valid[y_valid==1])
+        count_zeros= len(y_valid[y_valid==0])
+        guess = max(count_ones,count_zeros) / len(y_valid)
+        # PRC
+        plt.plot(prc_X, prc_Y, marker='.')
+        plt.plot([0, 1], [guess,guess], linestyle='--')
+        plt.xlabel('Recall')
+        plt.ylabel('Precision')
+        plt.show()
+        # ROC
+        fpr, tpr, roc_bins = roc_curve(y_valid, yhat_pred)
+        plt.plot(fpr, tpr, marker='.')
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.show()
+        
+    def get_statistics(self):
+        return \
+        self.cv_accuracy,\
+        self.cv_precision,\
+        self.cv_recall,\
+        self.cv_f1,\
+        self.cv_mcc,\
+        self.cv_auprc,\
+        self.cv_auroc
+
+class Separator():
+    
+    def __init__(self):
+        self.train_ids = []
+        self.train_seq = []
+        self.train_rci = dict()
+        self.val_ids = []
+        self.val_seq = []
+        self.val_rci = dict()
+        self.set_middle()
+        
+    def set_middle(self,ftest=False,ftailstest=False,fmiddletest=False,\
+                   ftrain=False,ftailstrain=False,fmiddletrain=False,\
+                   middlelow=0,middlehigh=0):
+        self.filter_test = ftest
+        self.filter_tails_test = ftailstest
+        self.filter_middle_test = fmiddletest
+        self.filter_train = ftrain
+        self.filter_tails_train = ftailstrain
+        self.filter_middle_train = fmiddletrain
+        self.middle_low = middlelow
+        self.middle_high = middlehigh
+        
+    def load(self,data_dir,rep,fold):
+        filename='cv.{}.{}.validation_genes.txt'.format(rep,fold)
+        filename = data_dir + filename
+        self.val_genes = set()
+        print('Opening file',filename)
+        with open(filename,'r') as fin:
+            for line in fin:
+                gene_id = line.strip()
+                self.val_genes.add(gene_id)
+                
+    def process(self,allids,allseq,gene_to_rci):
+        FILTER_TEST         = self.filter_test
+        FILTER_TAILS_TEST   = self.filter_tails_test
+        FILTER_MIDDLE_TEST  = self.filter_middle_test
+        FILTER_TRAIN        = self.filter_train
+        FILTER_TAILS_TRAIN  = self.filter_tails_train
+        FILTER_MIDDLE_TRAIN = self.filter_middle_train
+        MIDDLE_LOW          = self.middle_low
+        MIDDLE_HIGH         = self.middle_high
+        size = len(allids)
+        for t in range(size):
+            gene_id,tran_id = allids[t]
+            oneX            = allseq[t]
+            oneY            = gene_to_rci[gene_id]
+            in_middle = gene_to_rci[gene_id] >= MIDDLE_LOW and gene_to_rci[gene_id] <= MIDDLE_HIGH
+            in_tails = gene_to_rci[gene_id] < MIDDLE_LOW or gene_to_rci[gene_id] > MIDDLE_HIGH
+            if gene_id in self.val_genes:
+                if FILTER_TEST and (\
+                    (FILTER_TAILS_TEST and in_tails) or \
+                    (FILTER_MIDDLE_TEST and in_middle)):
+                    pass
+                else:
+                    self.val_ids.append(allids[t])
+                    self.val_seq.append(allseq[t])
+                    self.val_rci[gene_id]=gene_to_rci[gene_id]
+            else:
+                if FILTER_TRAIN and (\
+                    (FILTER_TAILS_TRAIN and in_tails) or \
+                    (FILTER_MIDDLE_TRAIN and in_middle)):
+                    pass
+                else:
+                    self.train_ids.append(allids[t])
+                    self.train_seq.append(allseq[t])
+                    self.train_rci[gene_id]=gene_to_rci[gene_id]
+                    
+    def get_ids(self):
+        return self.train_ids,self.val_ids
+    def get_seq(self):
+        return self.train_seq,self.val_seq
+    def get_rci(self):
+        return self.train_rci,self.val_rci
